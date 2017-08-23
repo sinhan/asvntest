6e6ae9bac5a454d4ef55b0fe833b3d3d3625ca3c
mansin <mansin@ae87d855-4922-4a4b-84c3-68634b31cb32>
2017-08-23 05:40:56 +0000

    create repo
    git-svn-id: file:///Users/mansin/svn/jmeter-svn/tags/v1_7_3@1 ae87d855-4922-4a4b-84c3-68634b31cb32

---
+++ src/core/org/apache/jmeter/reporters/AssertionReporter.java
@@ -0,0 +1,20 @@
+package org.apache.jmeter.reporters;
+import java.io.*;
+import java.util.*;
+import org.apache.jmeter.assertions.*;
+import org.apache.jmeter.samplers.*;
+import org.apache.jmeter.util.JMeterUtils;
+
+/****************************************
+ * Title: Jakarta-JMeter Description: Copyright: Copyright (c) 2001 Company:
+ * Apache
+ *
+ *@author    Michael Stover
+ *@created   $Date: 2002-08-11 12:24:54 -0700 (Sun, 11 Aug 2002) $
+ *@version   1.0
+ ***************************************/
+
+public class AssertionReporter extends ResultCollector implements Serializable
+{
+
+}
