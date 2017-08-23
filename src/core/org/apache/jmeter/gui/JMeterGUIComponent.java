08ef7794ac4427442c75330cc5a27e476536f1c7
mansin <mansin@ae87d855-4922-4a4b-84c3-68634b31cb32>
2017-08-23 05:40:56 +0000

    create repo
    git-svn-id: file:///Users/mansin/svn/jmeter-svn/tags/v1_8@1 ae87d855-4922-4a4b-84c3-68634b31cb32

---
+++ src/core/org/apache/jmeter/gui/JMeterGUIComponent.java
@@ -0,0 +1,82 @@
+package org.apache.jmeter.gui;
+import java.util.Collection;
+import javax.swing.JPopupMenu;
+import org.apache.jmeter.testelement.TestElement;
+/****************************************
+ * <p>
+ *
+ * Title: Jakarta JMeter</p> <p>
+ *
+ * Description: Load testing software</p> <p>
+ *
+ * Copyright: Copyright (c) 2002</p> <p>
+ *
+ * Company: Apache Foundation</p>
+ *
+ *@author    Michael Stover
+ *@created   $Date: 2002-08-19 15:33:14 -0700 (Mon, 19 Aug 2002) $
+ *@version   1.0
+ ***************************************/
+
+public interface JMeterGUIComponent
+{
+
+	/****************************************
+	 * !ToDo (Method description)
+	 *
+	 *@param name  !ToDo (Parameter description)
+	 ***************************************/
+	public void setName(String name);
+
+	/****************************************
+	 * !ToDoo (Method description)
+	 *
+	 *@return   !ToDo (Return description)
+	 ***************************************/
+	public String getName();
+
+	/****************************************
+	 * !ToDoo (Method description)
+	 *
+	 *@return   !ToDo (Return description)
+	 ***************************************/
+	public String getStaticLabel();
+
+	/****************************************
+	 * !ToDo (Method description)
+	 *
+	 *@return   !ToDo (Return description)
+	 ***************************************/
+	public TestElement createTestElement();
+	
+	/**
+	 * Test GUI elements can be turned disabled, in which case
+	 * they do not become part of the test when run.
+	 */
+	public boolean isEnabled();
+	
+	public void setEnabled(boolean enabled);
+
+	/****************************************
+	 * !ToDo (Method description)
+	 *
+	 *@return   !ToDo (Return description)
+	 ***************************************/
+	public JPopupMenu createPopupMenu();
+
+	/****************************************
+	 * !ToDo (Method description)
+	 *
+	 *@param element  !ToDo (Parameter description)
+	 ***************************************/
+	public void configure(TestElement element);
+
+	/****************************************
+	 * This is the list of menu categories this gui component will be available
+	 * under. For instance, if this represents a Controller, then the
+	 * MenuFactory.CONTROLLERS category should be in the returned collection.
+	 *
+	 *@return   !ToDo (Return description)
+	 ***************************************/
+	public Collection getMenuCategories();
+}
