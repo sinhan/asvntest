4a88bb3f7ce368d351032bb7983609d97dab4e12
mansin <mansin@ae87d855-4922-4a4b-84c3-68634b31cb32>
2017-08-23 05:40:56 +0000

    create repo
    git-svn-id: file:///Users/mansin/svn/jmeter-svn/tags/rel-2_1@1 ae87d855-4922-4a4b-84c3-68634b31cb32

---
+++ src/protocol/http/org/apache/jmeter/protocol/http/control/gui/HttpTestSampleGui2.java
@@ -0,0 +1,48 @@
+// $Header$
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package org.apache.jmeter.protocol.http.control.gui;
+
+import org.apache.jmeter.protocol.http.sampler.HTTPSamplerFactory;
+import org.apache.jmeter.protocol.http.sampler.HTTPSamplerBase;
+import org.apache.jmeter.testelement.TestElement;
+
+/**
+ * @version $Revision: 325652 $ on $Date: 2005-08-19 13:24:19 -0700 (Fri, 19 Aug 2005) $
+ */
+public class HttpTestSampleGui2 extends HttpTestSampleGui {
+
+	public HttpTestSampleGui2() {
+		super.init();
+	}
+
+	public TestElement createTestElement() {
+		HTTPSamplerBase sampler = HTTPSamplerFactory.newInstance("HTTPSampler2");
+		modifyTestElement(sampler);
+		return sampler;
+	}
+
+	public String getStaticLabel() {
+		return super.getStaticLabel() + " HTTPClient";
+	}
+
+    public String getDocAnchor() {
+        return super.getStaticLabel().replace(' ', '_');
+    }
+
+}
